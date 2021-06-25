7cbbc94ebed7d8d68bd1632cede3348cc19c72ae
lyn-teambeta <86309684+lyn-teambeta@users.noreply.github.com>
2021-06-25 11:58:03 +0900

    crieate title scene

---
+++ Assets/Scenes/Lyn_Script/EndScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndScript : MonoBehaviour
+{
+
+    // ボタンが押された場合、今回呼び出される関数
+    public void OnClick()
+    {
+        Debug.Log("終了 押された!");  // ログを出力
+    }
+}
---
+++ Assets/Scenes/Lyn_Script/StartScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartScript : MonoBehaviour
+{
+
+    // ボタンが押された場合、今回呼び出される関数
+    public void OnClick()
+    {
+        Debug.Log("スタート 押された!");  // ログを出力
+    }
+}
