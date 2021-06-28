ae0937720cff4bf4f19dcc657896ab6a95c1134c
guillermo6130 <ogs613@i.softbank.jp>
2021-06-28 15:37:22 +0900

    add laser の挙動

--- Assets/girerumo/Scripts/LaserBeam_Controller.cs
+++ Assets/girerumo/Scripts/LaserBeam_Controller.cs
@@ -6,17 +6,37 @@ public class LaserBeam_Controller : MonoBehaviour
 {
     private bool Laser_bool = true;
     LineRenderer Laser_lineRenderer;
+    float maxDistance = 100;
+    float width_of_laser;
+    public List<GameObject> currentHitObjects = new List<GameObject>();
+    private float currentHitDistance;
+    private Vector3 origin;
+    private Vector3 direction;
+
     // Start is called before the first frame update
     void Start()
     {
         Laser_lineRenderer = GetComponent<LineRenderer>();
+        width_of_laser = Laser_lineRenderer.endWidth;
+        origin = Laser_lineRenderer.GetPosition(0);
+        direction = transform.up;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Laser_on_off();   
+        Laser_on_off();
+        currentHitObjects.Clear();
+        currentHitDistance = maxDistance;
+        RaycastHit[] all_hit=Physics.SphereCastAll(origin,width_of_laser,transform.up,maxDistance);
+        foreach(RaycastHit hit_test in all_hit)
+        {
+            currentHitObjects.Add(hit_test.transform.gameObject);
+            currentHitDistance = hit_test.distance;
+        }
+
     }
+
     void Laser_on_off()
     {
         if (Input.GetKey(KeyCode.Space))
@@ -29,4 +49,11 @@ public class LaserBeam_Controller : MonoBehaviour
         }
         Laser_lineRenderer.enabled = Laser_bool;
     }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(origin + direction * currentHitDistance, width_of_laser);
+    }
+
 }
