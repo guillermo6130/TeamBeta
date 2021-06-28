3241ec355680540583e1cac7844d8824d704b73a
lyn-teambeta <86309684+lyn-teambeta@users.noreply.github.com>
2021-06-28 15:17:35 +0900

    create scene

---
+++ Assets/Scenes/Lyn_space/Lyn_Script/StartScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//ボタンを使用するためUIとSceneManagerを使用ためSceneManagementを追加
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StartScript : MonoBehaviour
+{
+    // ボタンが押された場合、今回呼び出される関数
+    public void OnClick()
+    {
+        Debug.Log("スタート 押された!");  // ログを出力
+    }
+}
