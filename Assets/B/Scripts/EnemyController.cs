3cc6a46bdf3d5cbc00bd1e0885d357e2ad2b7e4f
choibc2017 <choibc2017@gmail.com>
2021-07-01 23:05:10 +0900

    敵1の移動と発生のスクリプトを作成

---
+++ Assets/B/Scripts/EnemyController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+
+       float fallSpeed;
+       float rotSpeed;
+
+    // Start is called before the first frame update
+    void Start() {
+          this.fallSpeed = 0.01f + 0.1f * Random.value;
+          this.rotSpeed = 6f + 3f * Random.value;
+    }
+
+    // Update is called once per frame
+    void Update() {
+          transform.Translate( 0, -fallSpeed, 0, Space.World);
+
+          transform.Rotate(0, rotSpeed, 0 );
+
+          if (transform.position.y < -8.0f) {
+                Destroy (gameObject);
+          }
+    }
+}
