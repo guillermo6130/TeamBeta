1a9a93a8959f3952bdd22622965a163c712fe7a1
guillermo6130 <ogs613@i.softbank.jp>
2021-07-01 18:59:01 +0900

    laser and player

--- Assets/girerumo/Scripts/LaserBeam_Controller.cs
+++ Assets/girerumo/Scripts/LaserBeam_Controller.cs
@@ -12,6 +12,11 @@ public class LaserBeam_Controller : MonoBehaviour
     private float currentHitDistance;
     private Vector3 origin;
     private Vector3 direction;
+    private GameObject parentVec;
+
+    private float offsetparent_x;
+    private float offsetparent_y_0;
+    private float offsetparent_y_1;
 
     // Start is called before the first frame update
     void Start()
@@ -20,6 +25,10 @@ public class LaserBeam_Controller : MonoBehaviour
         width_of_laser = Laser_lineRenderer.endWidth;
         origin = Laser_lineRenderer.GetPosition(0);
         direction = transform.up;
+        parentVec = transform.root.gameObject;
+        offsetparent_x = parentVec.transform.position.x;
+        offsetparent_y_0 = -parentVec.transform.position.y + origin.y;
+        offsetparent_y_1 = -parentVec.transform.position.y + Laser_lineRenderer.GetPosition(1).y;
     }
 
     // Update is called once per frame
@@ -28,13 +37,14 @@ public class LaserBeam_Controller : MonoBehaviour
         Laser_on_off();
         currentHitObjects.Clear();
         currentHitDistance = maxDistance;
-        RaycastHit[] all_hit=Physics.SphereCastAll(origin,width_of_laser,transform.up,maxDistance);
+        origin = Laser_lineRenderer.GetPosition(0);
+        RaycastHit[] all_hit=Physics.SphereCastAll(origin,width_of_laser/2,transform.up,maxDistance);
         foreach(RaycastHit hit_test in all_hit)
         {
             currentHitObjects.Add(hit_test.transform.gameObject);
             currentHitDistance = hit_test.distance;
         }
-
+        change_laser_position();
     }
 
     void Laser_on_off()
@@ -51,10 +61,23 @@ public class LaserBeam_Controller : MonoBehaviour
         Laser_lineRenderer.enabled = Laser_bool;
     }
 
+    void change_laser_position()
+    {
+        float x = parentVec.transform.position.x + offsetparent_x;
+        float y0 = parentVec.transform.position.y + offsetparent_y_0;
+        float y1 = parentVec.transform.position.y + offsetparent_y_1;
+
+
+
+        Laser_lineRenderer.SetPosition(0, (new Vector3(x, y0, 0)));
+        Laser_lineRenderer.SetPosition(1, (new Vector3(x, y1, 0)));
+        Laser_lineRenderer.SetPosition(2, (new Vector3(x, 100, 0)));
+    }
+
     private void OnDrawGizmosSelected()/*デバッグ用*/
     {
         Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(origin + direction * currentHitDistance, width_of_laser);
+        Gizmos.DrawWireSphere(origin + direction * currentHitDistance, width_of_laser/2);
     }
 
 }
