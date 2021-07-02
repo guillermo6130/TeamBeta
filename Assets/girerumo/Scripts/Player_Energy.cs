003f60744d0ab846f8010844b00f512d9a7e2011
guillermo6130 <ogs613@i.softbank.jp>
2021-07-02 21:53:09 +0900

    energy script added

---
+++ Assets/girerumo/Scripts/Player_Energy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Energy : MonoBehaviour
+{
+    private int Energy_now;
+    private void Start()
+    {
+        Energy_now = 0;
+    }
+
+    public void addEnergy(int x)
+    {
+        Energy_now += x;
+        Debug.Log(Energy_now);
+    }
+
+    public void setEnergy(int x)
+    {
+        Energy_now = x;
+    }
+}
