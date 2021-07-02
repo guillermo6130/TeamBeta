2eaa5805c2fad8ee51cbd73bdb7e62861abcddc8
lyn-teambeta <86309684+lyn-teambeta@users.noreply.github.com>
2021-07-02 18:31:11 +0900

    operation update

---
+++ Assets/Lyn/Lyn_Script/Score.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+
+    // スコアを表示する
+    public Text scoreText;
+
+    // スコア
+    private int score;
+
+
+    // PlayerPrefsで保存するためのキー
+    private string ScoreKey = "Score";
+
+    void Start()
+    {
+        Initialize();
+    }
+
+    void Update()
+    {
+        // スコアを表示する
+        scoreText.text = score.ToString();
+    }
+
+    // ゲーム開始前の状態に戻す
+    private void Initialize()
+    {
+        // スコアを0に戻す
+        score = 000000000;
+
+    }
+
+    // ポイントの追加
+    public void AddPoint(int point)
+    {
+        score = score + point;
+    }
+}
